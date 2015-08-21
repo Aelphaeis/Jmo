d5123fce6e4e4dbdd9c1d265390f6020e34ac9b7
Joseph Morain <aelphaeis@live.com>
2015-08-21 14:09:37 -0400

    Added Strings with utilities for padding specific characters to the right and left.

---
+++ src/jmo/util/Strings.java
@@ -0,0 +1,19 @@
+package jmo.util;
+
+public class Strings {
+	public static String padRight(String s, int n, char padding){
+		String pad = "";
+		for(int i = 0; i < n; i++){
+			pad += padding;
+		}
+		return  s + pad;
+		
+	}
+	public static String padLeft(String s, int n,  char padding) {
+		String pad = "";
+		for(int i = 0; i < n; i++){
+			pad += padding;
+		}
+		return pad + s;
+	}
+}

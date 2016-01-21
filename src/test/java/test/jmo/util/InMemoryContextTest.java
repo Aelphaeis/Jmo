9442f9343b29e007fe34c11e87f41e390f3b5a78
Joseph Morain <aelphaeis@live.com>
2016-01-21 14:24:29 -0500

    Added test file for InMemoryContext

---
+++ src/test/java/test/jmo/util/InMemoryContextTest.java
@@ -0,0 +1,14 @@
+package test.jmo.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class InMemoryContextTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}

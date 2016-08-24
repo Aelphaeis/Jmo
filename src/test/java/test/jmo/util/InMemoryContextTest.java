d9a3733dff93fb4398472c9db419d4895085568c
Joseph Morain <aelphaeis@live.com>
2016-08-24 16:30:58 -0400

    Fixed broken test.

--- src/test/java/test/jmo/util/InMemoryContextTest.java
+++ src/test/java/test/jmo/util/InMemoryContextTest.java
@@ -2,7 +2,6 @@ package test.jmo.util;
 
 import static org.junit.Assert.*;
 
-import javax.naming.Context;
 import javax.naming.InitialContext;
 import javax.naming.NamingException;
 
@@ -13,8 +12,8 @@ public class InMemoryContextTest {
 	@Test
 	public void test() throws NamingException {
 		InitialContext ic = new InitialContext();
-		Context ctx = (Context) ic.lookup("java:comp/env");
-		assertNotNull(ctx);
+		ic.lookup("java:comp/env");
+		assertNotNull(ic);
 	}
 
 }

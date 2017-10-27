1d09a07ea48f5a1a2960a994c8ca54eef91465dc
Joseph Morain <morain@echoworx.com>
2017-10-27 11:03:06 -0400

    Optimizations to Strings

--- src/test/java/test/jmo/util/StringsTest.java
+++ src/test/java/test/jmo/util/StringsTest.java
@@ -31,4 +31,62 @@ public class StringsTest {
 		String value = "value";
 		assertFalse(Strings.isBlank(value));
 	}
+	
+	@Test
+	public void padLeftCorrect() {
+		String value = Strings.EMPTY;
+		value = Strings.padLeft(value, 3, 'p');
+		assertEquals("ppp", value);
+		
+		value = Strings.EMPTY;
+		value = Strings.padLeft(value, 0, 'p');
+		assertEquals("", value);
+	
+		value = "val";
+		value = Strings.padLeft(value, 3, 'p');
+		assertEquals("pppval", value);
+		
+		value = "val";
+		value = Strings.padLeft(value, 0, 'p');
+		assertEquals("val", value);
+	}
+	
+	@Test
+	public void padRightCorrect() {
+		String value = Strings.EMPTY;
+		value = Strings.padRight(value, 3, 'p');
+		assertEquals("ppp", value);
+		
+		value = Strings.EMPTY;
+		value = Strings.padRight(value, 0, 'p');
+		assertEquals("", value);
+	
+		value = "val";
+		value = Strings.padRight(value, 3, 'p');
+		assertEquals("valppp", value);
+		
+		value = "val";
+		value = Strings.padRight(value, 0, 'p');
+		assertEquals("val", value);
+	}
+	
+	
+	@Test
+	public void padCorrect() {
+		String value = Strings.EMPTY;
+		value = Strings.pad(value, 3, 'p');
+		assertEquals("pppppp", value);
+		
+		value = Strings.EMPTY;
+		value = Strings.pad(value, 0, 'p');
+		assertEquals("", value);
+	
+		value = "val";
+		value = Strings.pad(value, 3, 'p');
+		assertEquals("pppvalppp", value);
+		
+		value = "val";
+		value = Strings.pad(value, 0, 'p');
+		assertEquals("val", value);
+	}
 }

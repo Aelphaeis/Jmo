20928257439def799ada98c1a73dc4a2a1de7027
Joseph Morain <morain@echoworx.com>
2017-10-27 11:41:15 -0400

    Cannot instantiate streams

--- src/main/java/jmo/util/Streams.java
+++ src/main/java/jmo/util/Streams.java
@@ -5,19 +5,21 @@ import java.io.InputStream;
 import java.io.OutputStream;
 
 public class Streams {
-	public static void copyStream(InputStream input, OutputStream output) 
-			throws IOException {
-		copyStream(input, output, 1024);
-		
+	private Streams() {
+		throw new IllegalStateException("Utility class");
 	}
-	
-	public static void copyStream(InputStream input, OutputStream output, int bufferSize)
-		    throws IOException {
-		
+
+	public static void copyStream(InputStream input, OutputStream output) throws IOException {
+		copyStream(input, output, 1024);
+
+	}
+
+	public static void copyStream(InputStream input, OutputStream output, int bufferSize) throws IOException {
+
 		int bytesRead;
-	    byte[] buffer = new byte[bufferSize]; 
-	    while ((bytesRead = input.read(buffer)) != -1)  {
-	        output.write(buffer, 0, bytesRead);
-	    }
+		byte[] buffer = new byte[bufferSize];
+		while ((bytesRead = input.read(buffer)) != -1) {
+			output.write(buffer, 0, bytesRead);
+		}
 	}
 }

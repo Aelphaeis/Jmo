1ffb90a458c8a28522f0342606d66f1a9a2765f7
Joseph Morain <morain@echoworx.com>
2017-12-20 16:49:09 -0500

    Abstracted transversal logic.

--- src/main/java/jmo/structures/TreeNode.java
+++ src/main/java/jmo/structures/TreeNode.java
@@ -1,40 +1,81 @@
 package jmo.structures;
 
 import java.util.ArrayList;
+import java.util.Collection;
+import java.util.EnumMap;
 import java.util.List;
 
 import jmo.patterns.visitor.Visitor;
 
 public class TreeNode<T> {
+	private static final TreeTraversal DEFAULT = TreeTraversal.PREORDER;
+	
 	private T value = null;
-	private TreeNode<T> parent = null;
-	private List<TreeNode<T>> children = null;
+	private TreeNode<T> parent;
+	private List<TreeNode<T>> children;
+	
+	private EnumMap<TreeTraversal, Transveral<T>> transverals;
 	
 	public TreeNode(){
 		setChildren(new ArrayList<TreeNode<T>>());
+		transverals = new EnumMap<>(TreeTraversal.class);
+		transverals.put(TreeTraversal.PREORDER, new PreoderTransveral());
 	}
 	
+	public TreeNode(T value) {
+		this();
+		setValue(value);
+	}
+	
+	/**
+	 * @return whether or not this node has a parent.
+	 */
 	public boolean hasParent(){
 		return getParent() != null;
 	}
 	
+	/**
+	 * @return whether or not this node has a value.
+	 */
 	public boolean hasValue(){
 		return getValue() != null;
 	}
 
+	/**
+	 * Assume this tree node is tree node A.
+	 * Takes data and inserts it into tree node B.
+	 * Takes tree node B and inserts it as a child of tree node A.
+	 * @param child data we want to insert into tree.
+	 */
 	public void addChild(T child){
 		TreeNode<T> node = new TreeNode<>();
 		node.setValue(child);
 		node.setParent(this);
-		getChildren().add(node);
+		addChild(node);
+		
 	}
 	
-	public void addChildren(List<T> children){
+	public void addChild(TreeNode<T> child) {
+		getChildren().add(child);
+	}
+	
+	/**
+	 * Calls {@link #addChild(T)} on every object in iterable
+	 * @param children
+	 */
+	public void addChildren(Collection<T> children){
 		for(T v : children){
 			addChild(v);
 		}
 	}
 	
+	public void addChild(Collection<TreeNode<T>> children) {
+		getChildren().addAll(children);
+	}
+
+	/**
+	 * @return all direct children of this node
+	 */
 	public List<T> getChildrenValues(){
 		List<T> output = new ArrayList<>();
 		for(TreeNode<T> child : getChildren()){
@@ -44,27 +85,21 @@ public class TreeNode<T> {
 	}
 	
 	public void transverseNodes(Visitor<TreeNode<T>> visitor){
-		transverseNodes(this, visitor);
+		transverseNodes(visitor, DEFAULT);
 	}
 	
-	private void transverseNodes(TreeNode<T> n, Visitor<TreeNode<T>> visitor){
-		visitor.visit(n);
-		for(TreeNode<T> node : n.getChildren()){
-			n.transverseNodes(node, visitor);
-		}
+	public void transverseNodes(Visitor<TreeNode<T>> visitor, TreeTraversal transveralMethod){
+		Transveral<T> transveral = transverals.get(transveralMethod);
+		transveral.transverseNodes(this, visitor);
 	}
 	
 	public void transverse(Visitor<T> visitor){
-		transverse(this, visitor);
+		transverse(visitor, DEFAULT);
 	}
 	
-	private void transverse(TreeNode<T> n, Visitor<T> action){
-		if( n.getValue() != null){
-			action.visit(n.getValue());
-		}
-		for(TreeNode<T> node : n.getChildren()){
-			n.transverse(node, action);
-		}
+	private void transverse(Visitor<T> visitor, TreeTraversal transveralMethod){
+		Transveral<T> transveral = transverals.get(transveralMethod);
+		transveral.transverse(this, visitor);
 	}
 	
 	public int getLevel(){
@@ -97,5 +132,30 @@ public class TreeNode<T> {
 
 	public void setChildren(List<TreeNode<T>> children) {
 		this.children = children;
+	}
+	
+	protected interface Transveral<T> {
+		void transverse(TreeNode<T> n, Visitor<T> action);
+		void transverseNodes(TreeNode<T> n, Visitor<TreeNode<T>> visitor);
+	}
+	
+	protected class PreoderTransveral implements Transveral<T>{
+		@Override
+		public void transverse(TreeNode<T> n, Visitor<T> action) {
+			if( n.getValue() != null){
+				action.visit(n.getValue());
+			}
+			for(TreeNode<T> node : n.getChildren()){
+				transverse(node, action);
+			}
+		}
+		
+		@Override
+		public void transverseNodes(TreeNode<T> n, Visitor<TreeNode<T>> visitor) {
+			visitor.visit(n);
+			for(TreeNode<T> node : n.getChildren()){
+				transverseNodes(node, visitor);
+			}
+		}
 	}
 }

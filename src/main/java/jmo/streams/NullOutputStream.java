e3c8dc69326692d9f86b490469132b4baee88ca6
Joseph Morain <morain@echoworx.com>
2018-04-06 13:28:13 -0400

    modifications to NullOutputStream to make it more clear.

--- src/main/java/jmo/streams/NullOutputStream.java
+++ src/main/java/jmo/streams/NullOutputStream.java
@@ -1,19 +1,25 @@
 package jmo.streams;
 
-import java.io.IOException;
 import java.io.OutputStream;
 
 /**
- * 
  * @author morain
- *
  */
 public class NullOutputStream extends OutputStream {
-	
+
 	public static final NullOutputStream INSTANCE = new NullOutputStream();
 
 	@Override
-	public void write(int b) throws IOException {
-		// Do nothing 
+	public void write(int b) {
+		// Do nothing
+	}
+
+	@Override
+	public void write(byte[] b, int offset, int length) {
+		// do nothing
+	}
+	
+	private NullOutputStream() {
+		//We don't really need people creating instances of this.
 	}
 }

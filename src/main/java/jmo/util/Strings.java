1e0658f54e8b9ab9e7820343968e0f7e6d001f9f
Joseph Morain <morain@echoworx.com>
2018-06-14 16:10:07 -0400

    implemented translate command line

--- src/main/java/jmo/util/Strings.java
+++ src/main/java/jmo/util/Strings.java
@@ -1,5 +1,8 @@
 package jmo.util;
 
+import java.util.ArrayList;
+import java.util.StringTokenizer;
+
 public final class Strings {
 	
 	public static final String EMPTY = "";
@@ -103,6 +106,103 @@ public final class Strings {
 			return builder.toString();
 		}
 	}
+	
+	/**
+	 * Crack a command line.
+	 * @param toProcess the command line to process.
+	 * @return the command line broken into strings.
+	 * An empty or null toProcess parameter results in a zero sized array.
+	 */
+	public static String[] translateCommandline(String toProcess) {
+		return new CommandTranslator(toProcess).translate();
+	}
+	
+	
+	private static class CommandTranslator {
+		enum ParseState{
+			NORMAL,
+			SINGLE,
+			DOUBLE;
+		}
+		final String input;
+
+		ArrayList<String> result = new ArrayList<>();
+		
+		StringTokenizer tok;
+		StringBuilder curr ;
+		ParseState state;
+		boolean quoted;
+		
+		public CommandTranslator(String i) {
+			tok = new StringTokenizer(isBlank(i)? "" : i, "\"' ", true);
+			curr = new StringBuilder();
+			state = ParseState.NORMAL;
+			quoted = false;
+			input = i;
+		}
+		
+		public String[] translate() {
+			return isBlank(input)? new String[0] : parse();
+		}
+		
+		String[] parse() {
+			
+			while(tok.hasMoreTokens()) {
+				String next = tok.nextToken();
+				switch(state) {
+					case SINGLE:
+						handleQuote(next,"'");
+						break;
+					case DOUBLE:
+						handleQuote(next,"\"");
+						break;
+					default:
+						handleNoQuote(next);
+						break;
+				}
+			}
+			if(quoted || curr.length() != 0) {
+				result.add(curr.toString());
+			}
+			validateResult();
+		
+			return result.toArray(new String[result.size()]);
+		}
+		
+		void handleQuote(String token, String expect) {
+			if(expect.equals(token)) {
+				quoted = true;
+				state = ParseState.NORMAL;
+			}
+			else {
+				curr.append(token);
+			}
+		}
+		
+		void handleNoQuote(String token) {
+			if ("\'".equals(token)) {
+				state = ParseState.SINGLE;
+			} else if ("\"".equals(token)) {
+				state = ParseState.DOUBLE;
+			} else if (" ".equals(token)) {
+				if (quoted || curr.length() != 0) {
+					result.add(curr.toString());
+					curr.setLength(0);
+				}
+			} else {
+				curr.append(token);
+			}
+			quoted = false;
+		}
+		
+		void validateResult() {
+			if(state != ParseState.NORMAL) {
+				String err = "unbalance quotes in " + input;
+				throw new IllegalArgumentException(err);
+			}
+		}
+	}
+	
 	
 	private Strings() { }
 
--- src/test/java/jmo/util/StringsTest.java
+++ src/test/java/jmo/util/StringsTest.java
@@ -166,5 +166,72 @@ public class StringsTest {
 		String result = Strings.remove(value, 'c');
 		assertEquals("", result);
 	}
+	
+	@Test
+	public void translateCommandline_null_empty() {
+		String [] result = Strings.translateCommandline(null);
+		assertNotNull(result);
+		assertEquals(0, result.length);
+	}
+	
+	@Test
+	public void translateCommandline_empty_empty() {
+		String [] result = Strings.translateCommandline("");
+		assertNotNull(result);
+		assertEquals(0, result.length);
+	}
+
+	@Test
+	public void translateCommandline_nameOnly_nameElement() {
+		String [] result = Strings.translateCommandline("name");
+		assertNotNull(result);
+		assertEquals(1, result.length);
+		assertEquals("name", result[0]);
+	}
+	
+	@Test
+	public void translateCommandline_twoTokensSingles_Tokenized() {
+		String [] result = Strings.translateCommandline("name 'test quote'");
+		assertNotNull(result);
+		assertEquals(2, result.length);
+		assertEquals("name", result[0]);
+		assertEquals("test quote", result[1]);
+	}
+	
+	@Test
+	public void translateCommandline_twoTokensDoubles_Tokenized() {
+		String [] result = Strings.translateCommandline("name \"test quote\"");
+		assertNotNull(result);
+		assertEquals(2, result.length);
+		assertEquals("name", result[0]);
+		assertEquals("test quote", result[1]);
+	}
+	
+	@Test
+	public void translateCommandline_spaceBeforeQuote_spaceIgnored() {
+		String [] result = Strings.translateCommandline(" 'test'");
+		assertNotNull(result);
+		assertEquals(1, result.length);
+		assertEquals("test", result[0]);
+	}
+	
+	@Test
+	public void translateCommandline_spaceAfterQuote_spaceIgnored() {
+		String [] result = Strings.translateCommandline("'test' ");
+		assertNotNull(result);
+		assertEquals(1, result.length);
+		assertEquals("test", result[0]);
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void translateCommandline_singleQuote_exception() {
+		Strings.translateCommandline("'");
+	}
+	
+	@Test(expected=IllegalArgumentException.class)
+	public void translateCommandline_doubleQuote_exception() {
+		Strings.translateCommandline("\"");
+	}
+
 }
 

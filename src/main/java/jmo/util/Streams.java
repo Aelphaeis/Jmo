0a967e87fae519c2419a7e1b264f2ba5c251d413
Joseph <aelphaeis@live.com>
2018-12-16 13:25:29 -0500

    Read all lines from file utility.

--- src/main/java/jmo/util/Streams.java
+++ src/main/java/jmo/util/Streams.java
@@ -1,8 +1,14 @@
 package jmo.util;
 
+import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
+import java.nio.charset.CharsetDecoder;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Streams {
 	private Streams() {
@@ -15,11 +21,31 @@ public class Streams {
 	}
 
 	public static void copyStream(InputStream input, OutputStream output, int bufferSize) throws IOException {
-
 		int bytesRead;
 		byte[] buffer = new byte[bufferSize];
 		while ((bytesRead = input.read(buffer)) != -1) {
 			output.write(buffer, 0, bytesRead);
 		}
+	}
+	
+	public static List<String> readLines(InputStream is, boolean closeStream)
+			throws IOException {
+		List<String> lines = readLines(is);
+		if(closeStream) {
+			is.close();
+		}
+		return lines;
+	}
+	
+	public static List<String> readLines(InputStream is) throws IOException {
+		CharsetDecoder decoder = StandardCharsets.UTF_8.newDecoder();
+		InputStreamReader isr =  new InputStreamReader(is, decoder);
+		BufferedReader reader = new BufferedReader(isr);
+		List<String> lines = new ArrayList<>();
+		String line;
+		while((line =  reader.readLine())!= null) {
+			lines.add(line);
+		}
+		return lines;
 	}
 }

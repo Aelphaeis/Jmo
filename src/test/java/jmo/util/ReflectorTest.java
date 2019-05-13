c06800fd6c629a94a8a1d66882b64e975fac49ce
Joseph Morain <morain@echoworx.com>
2019-05-13 13:30:45 -0400

    import refactor.

--- src/test/java/jmo/util/ReflectorTest.java
+++ src/test/java/jmo/util/ReflectorTest.java
@@ -1,12 +1,13 @@
 package jmo.util;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 import java.util.List;
 
 import org.junit.Test;
-
-import jmo.util.Reflector;
 
 public class ReflectorTest {
 

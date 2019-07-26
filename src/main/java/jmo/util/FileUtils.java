cb4d02c4bd97eb731664d41fe1c35814fab83e50
Joseph Morain <morain@echoworx.com>
2019-07-26 13:27:58 -0400

    Added list dirs.

--- src/main/java/jmo/util/FileUtils.java
+++ src/main/java/jmo/util/FileUtils.java
@@ -3,6 +3,8 @@ package jmo.util;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.HashSet;
+import java.util.Set;
 
 public class FileUtils {
 	
@@ -14,6 +16,27 @@ public class FileUtils {
 		try (FileWriter writer = new FileWriter(location)){
 			writer.write(content);
 		}
+	}
+	
+	
+	public static Set<File> listFileTree(File dir, boolean recursive) {
+		Set<File> fileTree = new HashSet<>();
+		if (dir == null || !dir.exists()) {
+			return fileTree;
+		}
+		File[] children = dir.listFiles();
+		if (children == null) {
+			fileTree.add(dir);
+		} else {
+			for (File entry : children) {
+				if (entry.isFile()) {
+					fileTree.add(entry);
+				} else if (recursive) {
+					fileTree.addAll(listFileTree(entry, recursive));
+				}
+			}
+		}
+		return fileTree;
 	}
 
 	private FileUtils() {

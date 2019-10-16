f28d4b77a1ae7382bc21d804d5e003b5897b509c
Joseph Morain <morain@echoworx.com>
2019-10-16 15:42:21 -0400

    Distinct Result found test

--- src/test/java/jmo/util/XPathsTest.java
+++ src/test/java/jmo/util/XPathsTest.java
@@ -1,11 +1,14 @@
 package jmo.util;
 
+import static org.junit.Assert.assertEquals;
+
 import java.io.File;
 import java.io.IOException;
 
 import org.junit.Rule;
 import org.junit.Test;
 import org.w3c.dom.Document;
+import org.w3c.dom.Element;
 
 import jmo.serialization.Serializer;
 import jmo.util.XPaths.DuplicateElementException;
@@ -19,7 +22,15 @@ public class XPathsTest {
 	@Test(expected=DuplicateElementException.class)
 	public void distinct_multiple_exception() throws IOException {
 		File sample = new File(folder.getResourceFolder(), "sample.xml");
-		Document document = Serializer.deserialize(sample);
-		XPaths.distinct(document, "breakfast_menu/food");
+		Document doc = Serializer.deserialize(sample);
+		XPaths.distinct(doc, "breakfast_menu/food");
+	}
+	
+	@Test
+	public void distinct_uniqueXPath_found() throws IOException {
+		File sample = new File(folder.getResourceFolder(), "sample.xml");
+		Document doc = Serializer.deserialize(sample);
+		Element e = XPaths.distinct(doc, "breakfast_menu/food[1]/name");
+		assertEquals("Belgian Waffles", e.getTextContent());
 	}
 }

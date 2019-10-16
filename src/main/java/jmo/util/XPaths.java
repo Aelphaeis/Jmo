378e14ddd7f33bcdbcb199d9a28c042ada0be14b
Joseph Morain <morain@echoworx.com>
2019-10-16 15:11:26 -0400

    create XPath utility class

---
+++ src/main/java/jmo/util/XPaths.java
@@ -0,0 +1,18 @@
+package jmo.util;
+
+import org.w3c.dom.Element;
+
+public class XPaths {
+	
+	public static Element distinct() {
+		return null;
+	}
+	
+	public static Element first() {
+		return null;
+	}
+	
+	private XPaths() {
+		//utility class
+	}
+}

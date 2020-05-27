5ea16f0f1ed63d616dc7feaed0b5d27d67db549a
Joseph <aelphaeis@live.com>
2020-05-27 12:44:03 -0400

    functional interface candidate

--- src/test/java/test/jmo/performance/Candidate.java
+++ src/test/java/test/jmo/performance/Candidate.java
@@ -1,24 +1,33 @@
 package test.jmo.performance;
 
+@FunctionalInterface
 public interface Candidate {
-	String getName();
+
 	void doAction();
+
+	default String getName() {
+		return getClass().getName();
+	}
+
 	default void warmup() {
 		int limit = getWarmupIterations();
-		for(int i = 0; i < limit; i++) {
+		for (int i = 0; i < limit; i++) {
 			doAction();
 		}
 	}
+
 	default int getWarmupIterations() {
 		return 10000;
 	}
+
 	default int getPerformanceIterations() {
 		return 1000000;
 	}
+
 	default long checkPerformance() {
 		long start = System.currentTimeMillis();
 		int performanceCount = getPerformanceIterations();
-		for(int i = 0; i < performanceCount; i ++) {
+		for (int i = 0; i < performanceCount; i++) {
 			doAction();
 		}
 		long end = System.currentTimeMillis();

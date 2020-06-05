cf9e67aa155e235c464ffd0a2a85fad56e3f37af
Joseph <aelphaeis@live.com>
2020-06-05 15:47:42 -0400

    perf test tweaks

--- src/test/java/test/jmo/performance/PerformanceTest.java
+++ src/test/java/test/jmo/performance/PerformanceTest.java
@@ -6,6 +6,8 @@ import static jmo.util.NamingConventions.toScreamingSnakeCaseFromCamelCase3;
 import static jmo.util.NamingConventions.toScreamingSnakeCaseFromCamelCase4;
 import static org.junit.Assert.assertEquals;
 
+import java.util.LongSummaryStatistics;
+
 import org.junit.Before;
 import org.junit.Ignore;
 import org.junit.Test;
@@ -13,16 +15,11 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 public class PerformanceTest {
-	
-	private static final Logger logger = LoggerFactory.getLogger("debug");
 
+	private static final Logger logger = LoggerFactory
+			.getLogger(PerformanceTest.class);
 	private static final String LC_HELLO_WORLD = "HelloWorld!";
 	private static final String HELLO_WORLD = "HELLO_WORLD!";
-	
-	private static final String ITFORMAT = "Candidate [%s] run %s : %s ms";
-	private static final String TIFORMAT = "Canadidate [%s] ran %s times";
-	private static final String TOFORMAT = "Candidate [%s] total :  %s ms";
-	private static final String AVGFORMAT = "Candidate [%s] average : %s ms";
 	Candidate a;
 	Candidate b;
 	Candidate c;
@@ -51,35 +48,32 @@ public class PerformanceTest {
 
 	@Test
 	public void performanceTest() {
-		a.doAction();
-		b.doAction();
-		c.doAction();
-		d.doAction();
-
-		run(a);
-		run(b);
-		run(c);
-		run(d);
+		run(a, b, c, d);
 	}
 
-	void run(Candidate can) {
-		can.warmup();
-		int limit = 10;
-		long total = 0;
+	private void run(Candidate... candidates) {
+		for (int i = 0; i < candidates.length; i++) {
+			candidates[i].doAction();
+		}
 
-		if (logger.isDebugEnabled()) {
-			for (int i = 0; i < limit; i++) {
-				long dur = can.checkPerformance();
-				logger.debug(String.format(ITFORMAT, can.getName(), i, dur));
-				total += dur;
+		for (int i = 0; i < candidates.length; i++) {
+			run(candidates[i]);
+		}
+	}
+
+	private void run(Candidate can) {
+		can.warmup();
+
+		LongSummaryStatistics lss = new LongSummaryStatistics();
+
+		String name = this.c.getName();
+		for (int i = 0; i < 10; i++) {
+			long dur = can.checkPerformance();
+			lss.accept(dur);
+			if (logger.isDebugEnabled()) {
+				logger.debug(name + ":" + i + ":" + dur);
 			}
 		}
-
-		if (logger.isInfoEnabled()) {
-			logger.info(String.format(TIFORMAT, can.getName(), limit));
-			logger.info(String.format(TOFORMAT, can.getName(), total));
-			logger.info(String.format(AVGFORMAT, can.getName(), total / limit));
-		}
-
+		logger.info(can.getName() + " : " + lss);
 	}
 }

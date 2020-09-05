19740d7664ee88e20310d527236968670dc4cb77
Joseph <aelphaeis@live.com>
2020-09-05 02:39:55 -0400

    Refactor Exceptions to do to remove ambiguity

--- src/main/java/jmo/util/BufferedImages.java
+++ src/main/java/jmo/util/BufferedImages.java
@@ -5,6 +5,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.URL;
 import java.util.Objects;
+import java.util.function.Function;
 
 import javax.imageio.ImageIO;
 
@@ -24,6 +25,24 @@ public class BufferedImages {
 		}
 		catch (IOException e) {
 			String fmt = "unable to parse argument [%s] at [%s]";
+			String err = String.format(fmt, name, url);
+			throw new IllegalArgumentException(err, e);
+		}
+	}
+	public static <T> T fromClasspath(String name, Function<InputStream, T> f) {
+		Objects.requireNonNull(name);
+
+		URL url = BufferedImages.class.getClassLoader().getResource(name);
+		if (url == null) {
+			String fmt = "[%s] is not a resource";
+			throw new IllegalArgumentException(String.format(fmt, name));
+		}
+
+		try (InputStream stream = url.openStream()) {
+			return f.apply(stream);
+		}
+		catch (IOException e) {
+			String fmt = "error processing stream [%s] at [%s]";
 			String err = String.format(fmt, name, url);
 			throw new IllegalArgumentException(err, e);
 		}
--- src/main/java/jmo/util/Exceptions.java
+++ src/main/java/jmo/util/Exceptions.java
@@ -10,17 +10,17 @@ import java.util.function.Function;
 import java.util.function.Supplier;
 
 public class Exceptions {
-	
+
 	public static <T extends Throwable> boolean hasInstance(Throwable t,
 			Class<T> type) {
 		return hasInstance(t, type, false);
 	}
-	
+
 	public static <T extends Throwable> boolean hasInstance(Throwable t,
 			Class<T> type, boolean strict) {
 		return !getInstances(t, type, strict).isEmpty();
 	}
-	
+
 	/**
 	 * <p>
 	 * Given an exception and a type, searches the the exceptions (inclusive of
@@ -33,16 +33,14 @@ public class Exceptions {
 	 * occurrence in the trace
 	 * </p>
 	 * 
-	 * @param from
-	 *            Throwable who's causes we want to check
-	 * @param type
-	 *            Type of exception to return
+	 * @param from Throwable who's causes we want to check
+	 * @param type Type of exception to return
 	 */
 	public static <T extends Throwable> List<T> getInstances(Throwable t,
 			Class<T> type) {
 		return getInstances(t, type, false);
 	}
-	
+
 	/**
 	 * <p>
 	 * Given an exception and a type, searches the exceptions (inclusive of the
@@ -55,12 +53,9 @@ public class Exceptions {
 	 * occurrence in the trace
 	 * </p>
 	 * 
-	 * @param from
-	 *            Throwable who we want to check
-	 * @param type
-	 *            Type of exception to return
-	 * @param strict
-	 *            only return the exact class specified (no sub types)
+	 * @param from   Throwable who we want to check
+	 * @param type   Type of exception to return
+	 * @param strict only return the exact class specified (no sub types)
 	 */
 	public static <T extends Throwable> List<T> getInstances(Throwable t,
 			Class<T> type, boolean strict) {
@@ -72,10 +67,11 @@ public class Exceptions {
 		do {
 			if (strict) {
 				if (throwable.getClass() == type) {
-					
+
 					throwables.add(type.cast(throwable));
 				}
-			} else {
+			}
+			else {
 				if (type.isAssignableFrom(throwable.getClass())) {
 					throwables.add(type.cast(throwable));
 				}
@@ -84,7 +80,7 @@ public class Exceptions {
 		} while (throwable != null);
 		return throwables;
 	}
-	
+
 	/**
 	 * <p>
 	 * Given an exception and a type, searches the nested exceptions of the
@@ -103,7 +99,7 @@ public class Exceptions {
 			Class<T> type) {
 		return getCauseInstance(from, type, false);
 	}
-	
+
 	/**
 	 * 
 	 * <p>
@@ -121,23 +117,21 @@ public class Exceptions {
 	 * If no exception matches the criteria, returns null.
 	 * </p>
 	 * 
-	 * @param from
-	 *            Throwable who's causes we want to check
-	 * @param type
-	 *            Type of exception to return
-	 * @param strict
-	 *            only return the exact class specified (no sub types)
+	 * @param from   Throwable who's causes we want to check
+	 * @param type   Type of exception to return
+	 * @param strict only return the exact class specified (no sub types)
 	 */
 	public static <T extends Throwable> T getCauseInstance(Throwable from,
 			Class<T> type, boolean strict) {
 		List<T> throwables = getCauseInstances(from, type, strict);
 		if (throwables.isEmpty()) {
 			return null;
-		} else {
+		}
+		else {
 			return throwables.get(throwables.size() - 1);
 		}
 	}
-	
+
 	/**
 	 * <p>
 	 * Given an exception and a type, searches the nested exceptions of the
@@ -145,16 +139,14 @@ public class Exceptions {
 	 * assignable to the type specified and returns a list.
 	 * </p>
 	 * 
-	 * @param from
-	 *            Throwable who's causes we want to check
-	 * @param type
-	 *            Type of exception to return
+	 * @param from Throwable who's causes we want to check
+	 * @param type Type of exception to return
 	 */
 	public static <T extends Throwable> List<T> getCauseInstances(
 			Throwable from, Class<T> type) {
 		return getCauseInstances(from, type, false);
 	}
-	
+
 	/**
 	 * <p>
 	 * Given an exception and a type, searches the nested exceptions of the
@@ -162,18 +154,15 @@ public class Exceptions {
 	 * assignable to the type specified and returns a list.
 	 * </p>
 	 * 
-	 * @param from
-	 *            Throwable who's causes we want to check
-	 * @param type
-	 *            Type of exception to return
-	 * @param strict
-	 *            only return the exact class specified (no sub types)
+	 * @param from   Throwable who's causes we want to check
+	 * @param type   Type of exception to return
+	 * @param strict only return the exact class specified (no sub types)
 	 */
 	public static <T extends Throwable> List<T> getCauseInstances(
 			Throwable from, Class<T> type, boolean strict) {
 		return getInstances(from.getCause(), type, strict);
 	}
-	
+
 	/**
 	 * Get list of nested exceptions. The first exception is the outer must
 	 * exception and the last exception is the innermost exception
@@ -188,7 +177,7 @@ public class Exceptions {
 		}
 		return throwables;
 	}
-	
+
 	/**
 	 * Takes a throwable's stack trace and puts it into a string
 	 * 
@@ -201,96 +190,86 @@ public class Exceptions {
 		t.printStackTrace(pWriter);
 		return sWriter.toString();
 	}
-	
-	public static <T> Supplier<T> uncheck(ExceptionalSupplier<T> supplier) {
-		return () -> {
-			try {
-				return supplier.get();
-			}
-			catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		};
-	}
-	
-	/**
-	 * <p>
-	 * Takes a function wraps it in another function.
-	 * 
-	 * The wrapping function catches all subtypes of exception and wraps them in
-	 * an exception of type {@code AutomationException}
-	 * </p>
-	 * 
-	 * @param <T>
-	 *            the input argument
-	 * @param <R>The
-	 *            element type of the new stream
-	 * @param checked
-	 *            a non-interfering, stateless, function to apply to each
-	 *            element of a stream
-	 * @return the wrapped function
-	 * 
-	 * @throws RuntimeException
-	 *             if the function passed in throws any exception
-	 * 
-	 * 
-	 */
-	public static <T, R> Function<T, R> uncheck(ExceptionalFunction<T, R> f) {
-		return t -> {
-			try {
-				return f.apply(t);
-			}
-			catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		};
-	}
-	
-	public static <T> Consumer<T> uncheck(ExceptionalConsumer<T> consumer){
-		return c -> {
-			try {
-				consumer.accept(c);
-			}
-			catch (Exception e) {
-				throw new RuntimeException(e);
-			}
-		};
-	}
-	
+
 	@FunctionalInterface
-	public static interface ExceptionalSupplier<T> {
-		
+	public static interface CheckedSupplier<T> {
+
+		public static <T> Supplier<T> uncheck(CheckedSupplier<T> s) {
+			return () -> {
+				try {
+					return s.get();
+				}
+				catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			};
+		}
+
 		public T get() throws Exception;
 	}
-	
+
 	/**
 	 * Represents a function that accepts one argument and produces a result.
 	 *
-	 * <p>
-	 * This is a <a href="package-summary.html">functional interface</a> whose
-	 * functional method is {@link #apply(Object)}.
 	 *
-	 * @param <T>
-	 *            the type of the input to the function
-	 * @param <R>
-	 *            the type of the result of the function
+	 * @param <T> the type of the input to the function
+	 * @param <R> the type of the result of the function
 	 */
 	@FunctionalInterface
-	public static interface ExceptionalFunction<T, R> {
-		
+	public static interface CheckedFunction<T, R> {
+
+		/**
+		 * <p>
+		 * Takes a function wraps it in another function.
+		 * 
+		 * The wrapping function catches all sub types of exception and wraps
+		 * them in an exception of type {@code AutomationException}
+		 * </p>
+		 * 
+		 * @param <T>     the input argument
+		 * @param <R>     The element type of the new stream
+		 * @param checked a non-interfering, state-less, function to apply to
+		 *                each element of a stream
+		 * @return the wrapped function
+		 * 
+		 * @throws RuntimeException if the function passed in throws any
+		 *                          exception
+		 */
+		public static <T, R> Function<T, R> uncheck(CheckedFunction<T, R> f) {
+			return t -> {
+				try {
+					return f.apply(t);
+				}
+				catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			};
+		}
+
 		public R apply(T t) throws Exception;
 	}
-	
-	@FunctionalInterface
-	public interface ExceptionalConsumer<T> {
 
-	    /**
-	     * Performs this operation on the given argument.
-	     *
-	     * @param t the input argument
-	     */
-	    void accept(T t) throws Exception;
+	@FunctionalInterface
+	public interface CheckedConsumer<T> {
+
+		public static <T> Consumer<T> uncheck(CheckedConsumer<T> consumer) {
+			return c -> {
+				try {
+					consumer.accept(c);
+				}
+				catch (Exception e) {
+					throw new RuntimeException(e);
+				}
+			};
+		}
+
+		/**
+		 * Performs this operation on the given argument.
+		 *
+		 * @param t the input argument
+		 */
+		void accept(T t) throws Exception;
 	}
-	
+
 	private Exceptions() {}
 }
